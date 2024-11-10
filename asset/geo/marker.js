58baaa9dc789c4009b944f519a34010b1c8b3ca4
Reskipiandari <reskipiandari@gamil.com>
2024-11-10 20:37:45 +0800

    reski

---
+++ asset/geo/marker.js
@@ -0,0 +1,248 @@
+var markers = [
+    {
+        'kategori': 'kantor_camat',
+        'icon': getIcon('camat.png'),
+        'data': [
+            {
+                'name': 'Kantor Camat Lembar',
+                'latlang': [-8.716859655478748, 116.08230821751584]
+            },
+            {
+                'name': 'Kantor Camat Sekotong',
+                'latlang': [-8.773867297018363, 116.04952089656535]
+            },
+            {
+                'name': 'Kantor Camat Gerung',
+                'latlang': [-8.679867898003218, 116.1295150942269]
+            },
+            {
+                'name': 'Kantor Camat Kuripan',
+                'latlang': [-8.66815873644333, 116.1695121951856]
+            },
+            {
+                'name': 'Kantor Kecamatan Labuapi',
+                'latlang': [-8.630483312808952, 116.12316362679582]
+            },
+            {
+                'name': 'Kantor Camat Kediri',
+                'latlang': [-8.633538217201345, 116.14942781582948]
+            },
+            {
+                'name': 'Kantor Camat Narmada',
+                'latlang': [-8.587465317064995, 116.20211562465529]
+            },
+            {
+                'name': 'Kantor Camat Gunungsari',
+                'latlang': [-8.540253433551369, 116.10035216412128]
+            },
+            {
+                'name': 'Kantor Camat Lingsar',
+                'latlang': [-8.566054459671163, 116.18138817022427]
+            },
+            {
+                'name': 'Kantor Camat Batulayar',
+                'latlang': [-8.525196119455977, 116.06672971384123]
+            },
+        ]
+    },
+    {
+        'kategori': 'keamanan',
+        'icon': getIcon('keamanan.png'),
+        'data': [
+            {
+                'name': 'Polsek Labu Api Police Station',
+                'latlang': [-8.638691218636277, 116.10234618029631]
+            },
+            {
+                'name': 'Polsek Gerung',
+                'latlang': [-8.681541542175244, 116.1248338189941]
+            },
+            {
+                'name': 'Polsek Lembar Resort Lombok Barat',
+                'latlang': [-8.729784867941799, 116.08633786481208]
+            },
+            {
+                'name': 'Polsek Narmada',
+                'latlang': [-8.593405645689982, 116.19739276333027]
+            },
+            {
+                'name': 'Polsek Lingsar',
+                'latlang': [-8.56418950825059, 116.15297080437855]
+            },
+            {
+                'name': 'Polsek Gunung Sari',
+                'latlang': [-8.537993822440328, 116.09917540266284]
+            },
+            {
+                'name': 'Polsek Sekotong',
+                'latlang': [-8.777599698694353, 116.05037513331216]
+            },
+
+        ]
+    },
+    {
+        'kategori': 'kesehatan',
+        'icon': getIcon('kesehatan.png'),
+        'data': [
+            {
+                'name': 'Puskesmas Lingsar',
+                'latlang': [-8.567287723475824, 116.20005057605519]
+            },
+            {
+                'name': 'Puskesmas Pembantu Lingsar',
+                'latlang': [-8.568169055146022, 116.18222501349781]
+            },
+            {
+                'name': 'Puskesmas Sigerongan',
+                'latlang': [-8.563982710588354, 116.15303565597218]
+            },
+            {
+                'name': 'Rumah Sakit Umum Daerah Patut Patuh Patju',
+                'latlang': [-8.67699777531967, 116.12852550710969]
+            },
+            {
+                'name': 'Puskesmas Pembantu Sandik',
+                'latlang': [-8.536366303191576, 116.08908534300873]
+            },
+            {
+                'name': 'Puskesmas Sekotong',
+                'latlang': [-8.774246854934137, 116.05048705199256]
+            },
+
+            {
+                'name': 'Poskesdes Blongas',
+                'latlang': [-8.846598270160506, 116.01694707161691]
+            },
+            {
+                'name': 'Rumah Sakit Umum Daerah Patut Patuh Patju',
+                'latlang': [-8.678498705174578, 116.12486797541295]
+            },
+        ]
+    },
+    {
+        'kategori': 'pendidikan',
+        'icon': getIcon('pendidikan.png'),
+        'data': [
+            {
+                'name': 'SD Negeri 1 Sekotong Tengah',
+                'latlang': [-8.77564140007199, 116.04985967374027]
+            },
+            {
+                'name': 'SMPN 1 SEKOTONG',
+                'latlang': [-8.77741061281111, 116.04806949469086]
+            },
+            {
+                'name': 'Yayasan Darul Habib Bintaur',
+                'latlang': [-8.837205042670412, 116.09640420200277]
+            },
+            {
+                'name': 'SDI TARBIATUL ULUM',
+                'latlang': [-8.752110064241315, 116.08387298404018]
+            },
+            {
+                'name': 'TK & SD IT Imam Bukhori Lembar',
+                'latlang': [-8.73211610647679, 116.08995957637644]
+            },
+            {
+                'name': 'SD 2 Jembatan Kembar',
+                'latlang': [-8.708228115193249, 116.08960154154366]
+            },
+            {
+                'name': 'SMP Negeri 1 Narmada',
+                'latlang': [-8.592894005482723, 116.19879365367713]
+            },
+        ]
+    },
+    {
+        'kategori': 'pariwisata',
+        'icon': getIcon('pariwisata.png'),
+        'data': [
+            {
+                'name': 'Ekowisata Mangrove Bagek Kembar',
+                'latlang': [-8.76526905347956, 116.05223875907218]
+            },
+            {
+                'name': 'Mangrove Sekotong Tengah',
+                'latlang': [-8.76315921595951, 116.0467437196394]
+            },
+            {
+                'name': 'Skyland Resort Lombok',
+                'latlang': [-8.761479151596836, 116.02235207053546]
+            },
+            {
+                'name': 'LENDANG RE AM',
+                'latlang': [-8.758939504925007, 116.03903485110538]
+            },
+            {
+                'name': 'PT. PANTAI DAMAI SEJAHTERA',
+                'latlang': [-8.744539129288594, 116.06581267457514]
+            },
+            {
+                'name': 'View Point Jurang Gerepek',
+                'latlang': [-8.737857564039635, 116.08775329876373]
+            },
+            {
+                'name': 'Labuan Tereng',
+                'latlang': [-8.734067383077525, 116.07743527547926]
+            },
+            {
+                'name': 'Bukit Batu Idung (BBI)',
+                'latlang': [-8.705325687422361, 116.10615128488384]
+            },
+            {
+                'name': 'Wisata Kuliner Mewah (Mepet Sawah) Tempos',
+                'latlang': [-8.698900984650527, 116.1485394758823]
+            },
+            {
+                'name': 'Bundaran Gerung',
+                'latlang': [-8.664075319640455, 116.13139580798415]
+            },
+            {
+                'name': 'Taman Mekarsari Narmada',
+                'latlang': [-8.602889810342598, 116.16596573371112]
+            },
+            {
+                'name': 'Bukit Korea Dopang',
+                'latlang': [-8.521945856532609, 116.12800475426427]
+            },
+            {
+                'name': 'Bukit Bengkaung Lembah Sari',
+                'latlang': [-8.49846982903857, 116.0910799286021]
+            },
+            {
+                'name': 'Air Terjun Timponan',
+                'latlang': [-8.500063056914449, 116.21125441067693]
+            },
+
+            {
+                'name': 'Tibu Sendalem Waterfall',
+                'latlang': [-8.489577736316207, 116.26618984715365]
+            },
+
+            {
+                'name': 'Pantai Mekaki',
+                'latlang': [-8.829295256921297, 115.92810670686062]
+            },
+            {
+                'name': 'Buwun Mas Hills (Bukit Buwun Mas)',
+                'latlang': [-8.83663404838043, 116.04581138123149]
+            },
+            {
+                'name': 'BELONGAS BEACH',
+                'latlang': [-8.853090200827463, 116.02015491301299]
+            },
+        ]
+    }
+];
+
+
+function getIcon(icon) {
+    var greenIcon = L.icon({
+        iconUrl: 'asset/img/' + icon,
+        iconSize: [30, 30], // size of the icon
+        iconAnchor: [22, 44], // point of the icon which will correspond to marker's location
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+    return greenIcon;
+}
